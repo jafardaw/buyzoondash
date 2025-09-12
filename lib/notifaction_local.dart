2be5baf4639cd1971852db0833ab7008b2570626
jafardaw <jafardaw003g@gmail.com>
2025-09-12 20:08:26 +0300

    حفظ التغييsdسحب

--- lib/notifaction_local.dart
+++ lib/notifaction_local.dart
@@ -186,3 +186,77 @@ void saveTokenToDatabase(String? token) async {
 // void showLocalNotification(RemoteMessage message) {
 //   // تنفيذ عرض إشعار محلي باستخدام awesome_notifications أو أي مكتبة أخرى
 // }
+
+///////////////////////////////
+///
+///
+///main
+///
+//import 'package:buyzoonapp/core/util/api_service.dart';
+// import 'package:buyzoonapp/core/util/app_router.dart';
+// import 'package:buyzoonapp/firebase_options.dart';
+// import 'package:buyzoonapp/product_type/presentation/manger/add_product_type_cubit.dart';
+// import 'package:buyzoonapp/product_type/presentation/manger/delete_product_type_cubit.dart';
+// import 'package:buyzoonapp/product_type/presentation/manger/product_type_cubit.dart';
+// import 'package:buyzoonapp/product_type/presentation/manger/update_product_type_cubit.dart';
+// import 'package:buyzoonapp/product_type/repo/product_type_repo.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:flutter_localizations/flutter_localizations.dart';
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+
+//   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+//   // await initializeLocalNotifications();
+
+//   // await setupNotifications();
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MultiBlocProvider(
+//       providers: [
+//         BlocProvider<GetProductTypeCubit>(
+//           create: (context) =>
+//               GetProductTypeCubit(ProductTypeRepo(ApiService()))
+//                 ..getProductTypes(),
+//         ),
+//         BlocProvider<DeleteProductTypeCubit>(
+//           create: (context) =>
+//               DeleteProductTypeCubit(ProductTypeRepo(ApiService())),
+//         ),
+//         // Add AddProductTypeCubit and UpdateProductTypeCubit here to make them available
+//         BlocProvider<AddProductTypeCubit>(
+//           create: (context) =>
+//               AddProductTypeCubit(ProductTypeRepo(ApiService())),
+//         ),
+//         BlocProvider<UpdateProductTypeCubit>(
+//           create: (context) =>
+//               UpdateProductTypeCubit(ProductTypeRepo(ApiService())),
+//         ),
+//       ],
+//       child: MaterialApp.router(
+//         debugShowCheckedModeBanner: false,
+//         title: 'Splash Screen Demo',
+//         theme: ThemeData(primarySwatch: Colors.blue),
+//         localizationsDelegates: const [
+//           GlobalMaterialLocalizations.delegate,
+//           GlobalWidgetsLocalizations.delegate,
+//           GlobalCupertinoLocalizations.delegate,
+//         ],
+//         supportedLocales: const [
+//           Locale('ar'), // دعم اللغة العربية
+//           Locale('en'), // دعم اللغة الإنجليزية
+//         ],
+//         locale: const Locale('ar'),
+//         routerConfig: AppRoutes.router,
+//       ),
+//     );
+//   }
+// }
